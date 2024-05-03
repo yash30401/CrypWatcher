83af5080b5e3f87e99defb0a4fd8ae022eee5f77
Yashveer singh <yashveersingh30401@gmail.com>
2024-05-03 15:29:10 +0530

    CoinDetail Screen Created

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,6 +78,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7") // Check for the latest version on the Navigation Releases page
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
 
+    implementation("com.google.accompanist:accompanist-flowlayout:0.17.0")
+
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
--- app/src/main/java/com/yash/crypwatcher/presentation/MainActivity.kt
+++ app/src/main/java/com/yash/crypwatcher/presentation/MainActivity.kt
@@ -10,8 +10,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.yash.crypwatcher.presentation.coin_detail.CoinDetailScreen
+import com.yash.crypwatcher.presentation.coin_list.CoinListScreen
 import com.yash.crypwatcher.presentation.ui.theme.CrypWatcherTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +29,24 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ){
+                        composable(
+                            route = Screen.CoinListScreen.route
+                        ){
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(
+                            route = Screen.CoinDetailScreen.route + "/{coinId}"
+                        ){
+                            CoinDetailScreen()
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CrypWatcherTheme {
-        Greeting("Android")
     }
 }
